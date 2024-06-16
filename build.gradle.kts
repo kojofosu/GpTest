df5f0e89c5f401cfd55b8948726b663f613ca857
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2024-06-16 12:34:31 +0000

    init

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    `maven-publish`
+}
+
+
+publishing {
+    repositories {
+        maven {
+            name = "GPTest"
+            url = uri("https://maven.pkg.github.com/kojofosu/REPOSITORY")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
---
+++ lib/src/main/java/com/example/lib/MyClass.kt
@@ -0,0 +1,6 @@
+package com.example.lib
+
+class MyClass (
+    var gpName: String?,
+    var gpOparebea: String?
+)
